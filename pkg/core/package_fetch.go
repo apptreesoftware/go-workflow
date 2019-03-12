426522ab226c360461a8541521e8efae9fdee114
Matthew Smith <matthew@apptreesoftware.com>
2019-03-11 17:36:43 -0700

    added executables section of a package to define what os/arch a package supports

--- pkg/core/package_fetch.go
+++ pkg/core/package_fetch.go
@@ -1,7 +1,56 @@
 package core
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type PackageFetcher interface {
 	GetStepPackage(context.Context, *StepPackageRequest) (*StepPackageResponse, error)
+}
+
+func (pkg *Package) GetOSArchBinaryPath(os, arch string) (string, error) {
+	binary := pkg.getOSBinaries(os)
+	if binary == nil {
+		return "", NewExecutableNotFoundError(os, arch)
+	}
+	path := ""
+	switch arch {
+	case `amd64`:
+		path = binary.Amd64
+	case `i386`:
+		path = binary.I386
+	}
+	if path == "" {
+		return "", NewExecutableNotFoundError(os, arch)
+	}
+	return path, nil
+}
+
+func (pkg *Package) getOSBinaries(os string) *Binary {
+	var binary *Binary
+	switch os {
+	case `darwin`:
+		binary = pkg.Executables.Darwin
+	case `linux`:
+		binary = pkg.Executables.Linux
+	case `windows`:
+		binary = pkg.Executables.Windows
+	}
+	return binary
+}
+
+type ExecutableNotFound struct {
+	os, arch string
+}
+
+func NewExecutableNotFoundError(os, arch string) error {
+	return ExecutableNotFound{
+		os:   os,
+		arch: arch,
+	}
+}
+
+func (e ExecutableNotFound) Error() string {
+	return fmt.Sprintf("Executable not found OS: %s, ARCH: %s", e.os, e.arch)
 }
