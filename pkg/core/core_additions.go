3e0fba89c56e3282753f572e97f7f73b3f6c07bb
Matthew Smith <matthew@apptreesoftware.com>
2019-05-15 07:19:55 -0700

    Move yaml parsing

--- pkg/core/core_additions.go
+++ pkg/core/core_additions.go
@@ -2,8 +2,13 @@ package core
 
 import (
 	"context"
+	"fmt"
 	"github.com/twitchtv/twirp"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -23,4 +28,15 @@ func CopyTwirpClientAuthorization(ctx context.Context) context.Context  {
 		return outCtx
 	}
 	return ctx
+}
+
+func ReadPackageYaml(dir string) (Package, error) {
+	yamlFile := filepath.Join(dir, "package.yaml")
+	b, err := ioutil.ReadFile(yamlFile)
+	if err != nil && os.IsNotExist(err) {
+		return Package{}, fmt.Errorf("Could not find a package.yaml in %s", dir)
+	}
+	pkg := Package{}
+	err = yaml.Unmarshal(b, &pkg)
+	return pkg, err
 }
