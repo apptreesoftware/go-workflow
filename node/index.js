d4c7a5c45455f98356ae5be236f5bebf4b0f5679
Matthew Smith <matthew@apptreesoftware.com>
2019-08-27 13:27:11 -0700

    Node SDK improvements

--- node/index.js
+++ node/index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 const service = require('./gen/step_grpc_pb');
-var core = require('./gen/common_pb');
 const yaml = require('yaml');
 const grpc = require('grpc');
 const fs = require("fs");
 const steps = {};
+const yargs = require('yargs');
 
 module.exports.addStep = function (name, version, func) {
     steps[`${name}-${version}`] = func;
@@ -36,31 +36,48 @@ function _runServeMode(port) {
 
 function _runIPCMode() {
     const env = process.env;
-    const name = env['STEP_NAME'];
-    const version = env['STEP_VERSION'];
+    let name = env['STEP_NAME'];
+    let version = env['STEP_VERSION'];
     const outputPath = env['WORKFLOW_OUTPUT'];
-    module.exports.debug = env['DEBUG'] == 'true';
+    module.exports.debug = env['DEBUG'] === 'true';
+
+    if (version === undefined || version == null) {
+        version = '1.0';
+    }
+    const args = yargs.argv;
+    if (args['step'] != null) {
+        name = args['step'];
+    }
+    let fileName = args['file'];
+    let input = null;
+    if (fileName != null) {
+        input = fs.readFileSync(fileName, 'utf8');
+    }
+
     const stepId = `${name}-${version}`;
     const step = steps[stepId];
     if (step == null) {
         console.error(`${stepId} not found. Make sure this step is registered using 'addStep'`);
     }
-    let dataStr = "";
-    process.stdin.setEncoding("utf8");
-    process.stdin.on('data', (data) => {
-        console.log(`Input: ${data}`);
-        dataStr += data;
-    });
-    process.stdin.on('end', () => {
-        _runIPCWithInput(step, dataStr, outputPath);
-    });
+    if (input == null) {
+        let dataStr = "";
+        process.stdin.setEncoding("utf8");
+        process.stdin.on('data', (data) => {
+            console.log(`Input: ${data}`);
+            dataStr += data;
+        });
+        process.stdin.on('end', () => {
+            _runIPCWithInput(step, dataStr, outputPath);
+        });
+    } else {
+        _runIPCWithInput(step, input, outputPath);
+    }
 }
 
 function _runIPCWithInput(step, input, outputPath) {
     try {
         const parsedData = JSON.parse(input);
         const resp = step(parsedData);
-
         const outData = JSON.stringify(resp);
         if (outputPath === undefined || outputPath === null) {
             process.stdout.write(outData);
@@ -104,10 +121,10 @@ function runStep(call, callback) {
 }
 
 function getPackageInfo(call, callback) {
-    const file = fs.readFileSync('./package.yaml', 'utf8')
+    const file = fs.readFileSync('./package.yaml', 'utf8');
     const parsed = yaml.parse(file);
     console.log(parsed);
-    var pkg = new core.Package();
+    const pkg = new proto.core.Package();
 
     pkg.setName(parsed['name']);
     pkg.setVersion(`${parsed['version']}`);
@@ -116,7 +133,7 @@ function getPackageInfo(call, callback) {
     const map = pkg.getStepsMap();
     Object.keys(parsed.steps).forEach((k) => {
         let stepDef = parsed.steps[k];
-        let step = new core.PackageStep();
+        let step = new proto.core.PackageStep();
         step.description = stepDef['description'];
         step.version = stepDef['version'];
         let stepInputMap = step.getInputsMap();
@@ -125,7 +142,7 @@ function getPackageInfo(call, callback) {
         if (stepDef.inputs != null) {
             Object.keys(stepDef.inputs).forEach((inputKey) => {
                 let inputDef = stepDef.inputs[inputKey];
-                let input = new core.InputInfo();
+                let input = new proto.core.InputInfo();
                 input.setRequired(inputDef.required);
                 input.setDescription(inputDef.description);
                 input.setSample(inputDef.sample);
@@ -138,7 +155,7 @@ function getPackageInfo(call, callback) {
         if (stepDef.outputs != null) {
             Object.keys(stepDef.outputs).forEach((outputKey) => {
                 let outputDef = stepDef.outputs[outputKey];
-                let output = new core.OutputInfo();
+                let output = new proto.core.OutputInfo();
                 output.setDescription(outputDef['description']);
                 stepOutputMap.set(outputKey, output);
             });
